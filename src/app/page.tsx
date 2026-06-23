6de2ec6cee5cbf03a24bb748e8c0dab3e3906a33
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-06-23 17:02:03 +0530

    redirect to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function RootPage() {
   // This instantly pushes the user to localhost:3000/dashboard
-  redirect('/dashboard');
+  redirect('/login');
 }
